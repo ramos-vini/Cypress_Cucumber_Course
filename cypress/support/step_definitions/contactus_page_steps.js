f713ab09093caca8d6f839ce684f4cd775648bb5
ramos-vini <ramoscontrole@gmail.com>
2024-03-02 11:18:20 +0100

    Step definitions structure defined for the Scenario 'Submit valid form'

--- cypress/support/step_definitions/contactus_page_steps.js
+++ cypress/support/step_definitions/contactus_page_steps.js
@@ -1,7 +1,31 @@
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then} from "@badeball/cypress-cucumber-preprocessor";
 
 const baseUrl = 'https://www.webdriveruniversity.com/Contact-Us/contactus.html'
 
 Given(`I am on the 'Contact us' page`, () => {
-    cy.visit(url);
+    cy.visit(baseUrl);
 })
+
+When(`I type in my first name`, () => {
+
+})
+
+When(`I type in my last name`, () => {
+
+})
+
+When(`I type in my email address`, () => {
+
+})
+
+When(`I type in a comment`, () => {
+
+}) 
+
+When(`I submit the form`, () => {
+
+}) 
+
+Then(`I should see the message 'Thank You for your Message!'`, () => {
+
+}) 
