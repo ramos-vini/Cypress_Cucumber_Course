d263684c9937e6b12885444ecc7ebf04120dccf8
ramos-vini <ramoscontrole@gmail.com>
2024-03-02 11:43:29 +0100

    Finished 'Valid form submission' scenario step implementations

--- cypress/support/step_definitions/contactus_page_steps.js
+++ cypress/support/step_definitions/contactus_page_steps.js
@@ -7,25 +7,26 @@ Given(`I am on the 'Contact us' page`, () => {
 })
 
 When(`I type in my first name`, () => {
-
+    cy.get('[name="first_name"]').click().type('Vinicius');
 })
 
 When(`I type in my last name`, () => {
-
+    cy.get('[name="last_name"]').click().type('Ramos');
 })
 
 When(`I type in my email address`, () => {
-
+    cy.get('[name="email"]').click().type('viniciusramos@email.com');
 })
 
 When(`I type in a comment`, () => {
-
+    cy.get('textarea[name="message"]').click().type('My comment here.');
 }) 
 
 When(`I submit the form`, () => {
-
+    cy.get('input[type="submit"]').click();
 }) 
 
 Then(`I should see the message 'Thank You for your Message!'`, () => {
-
+    cy.contains('Thank You for your Message!');
+    // cy.get('h1').should('have.text', 'Thank You for your Message!'); // Exact assertion
 }) 
