299ab6ac4d8439556f3e12c796418f50ddb838f2
ramos-vini <ramoscontrole@gmail.com>
2024-03-15 17:02:09 +0100

    Moving step implementations from contactUsSteps to contactUsPage

--- cypress/support/pages/contactUsPage.js
+++ cypress/support/pages/contactUsPage.js
@@ -2,8 +2,44 @@ import BasePage from "./basePage";
 
 class ContactUsPage extends BasePage {
 
+    elements = {
+        first_name_field: () => cy.get('[name="first_name"]'),
+        last_name_field: () => cy.get('[name="last_name"]'),
+        email_field: () => cy.get('[name="email"]'),
+        comment_field: () => cy.get('textarea[name="message"]'),
+        submit_button: () => cy.get('input[type="submit"]')
+    };
+
+    navigateToContactUsPage(){
+        super.navigateTo('/Contact-Us/contactus.html');
+    }
+
     assertThatIamOnContactUsPage(){
         cy.url().should('eq', 'https://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    }
+
+    typeFirstName(firstName){
+        this.elements.first_name_field().click().type(firstName);
+    }
+
+    typeLastName(lastName){
+        this.elements.last_name_field().click().type(lastName);
+    }
+
+    typeEmail(email){
+        this.elements.email_field().click().type(email);
+    }
+
+    typeComment(comment){
+        this.elements.comment_field().click().type(comment);
+    }
+    
+    submitContactUsForm(){
+        this.elements.submit_button().click();
+    }
+
+    assertThatMessageIsDisplayed(message){
+        cy.contains(message);
     }
 }
 
